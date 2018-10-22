fbbc56a66b431736554971142d99e3da5f3080f4
Alberto Gil <alberto@thehyve.nl>
2018-10-22 14:16:51 +0200

    added script for preparing input for usagi.

--- python/preparation_semantic_mapping_input/wr_output_to_usagi_input.py
+++ python/preparation_semantic_mapping_input/wr_output_to_usagi_input.py
@@ -15,6 +15,7 @@
 import pandas as pd
 import sys
 import os
+from translate import Translator
 
 # dependency pip install xlrd
 
@@ -37,14 +38,17 @@ def available_mapping_tables(mapping_tables_dir, ini_file = 'tba_', end_file = '
     """
     Retrieves the list of available mapping tables
     """
-    return ['cod_' + x.split(end_file)[0].split(ini_file)[1] for x in os.listdir(mapping_tables_dir)]
+    available_tables = ['cod_' + x.split(end_file)[0].split(ini_file)[1] for x in os.listdir(mapping_tables_dir)]
+    # Add available table for cie9 and cie9p fields (they are the only columns without format col_NAME)
+    available_tables += ['cie9', 'cie9p']
+    return available_tables
 
 def create_mapping_dictionary(column_name, mapping_tables_dir, ini_file = 'tba_', end_file = '.csv',
                               col_code = 0):
     """
     Creates a mapping dictionary for a specified column
     """
-    d =  import_csv(mapping_tables_dir + ini_file + column_name + end_file)
+    d =  import_csv(mapping_tables_dir + ini_file + column_name.split('cod_')[-1] + end_file)
     code_index = col_code
     if col_code == 0:
         translated_code_index = 1
@@ -76,8 +80,11 @@ def split_white_rabbit(filename, path = '', n_sheet = 1):
 
 
 def translate_values(values_list, col_name, mapping_tables_dir, col_code = 0, ini_col = 'cod_'):
+
+    #if col_name == 'cie9p':
+    #    col_code = 1
     # Generate dictionary
-    translator = create_mapping_dictionary(col_name.split(ini_col)[1], mapping_tables_dir, col_code = col_code)
+    translator = create_mapping_dictionary(col_name.split(ini_col)[-1], mapping_tables_dir, col_code = col_code)
 
 
     translated_list = []
@@ -91,41 +98,70 @@ def translate_values(values_list, col_name, mapping_tables_dir, col_code = 0, in
 
 
 
-def csv_file_from_sheet(sheet_df, sheet_name):
+def csv_file_from_sheet(sheet_df, sheet_name, translate_descriptions = False):
     """
     Splits a WR sheet from a specific file in multiple csv files for each column
     """
     # Retrieve list of available mapping columns
     avail_map = available_mapping_tables(mapping_tables_dir)
 
+    # Iterate through each colum name
+    # Fomat: column1, frequency1, column2, frequency2, column3, frequency3, ...
     for ix in range(0, len(sheet_df.columns), 2):
+        # Retrieve column name and data
         col_name = sheet_df.columns[ix]
         subset_df = sheet_df.iloc[:,[ix,ix+1]].dropna(0)
 
-        #print(avail_map)
+        # Check if the retrieved column has an available mapping table
         if col_name in avail_map:
+            # Map values from ABUCASIS to their descriptions
             values_to_translate = subset_df.iloc[:,0].values.tolist()
-            translated_values  = translate_values(values_to_translate, col_name, mapping_tables_dir)
-            if [x[0] for x in translated_values].count('Unknown') == len(translated_values):
+
+            # print(subset_df.head())
+            values_to_translate = subset_df.iloc[:,0].values.tolist()
+            #print([[x,1] for x in missing_values_wr])
+
+            translated_values  = translate_values(values_to_translate, col_name, mapping_tables_dir, col_code = 0)
+
+            # Check if the translation was succesful
+            if [x[0] for x in translated_values].count('Unknown') > 0.1*len(translated_values):
+                # If it was not succesful, try using next column from the tba tables
                 #print(translated_values.count('Unknown'))
-                n_translated_values = translate_values(values_to_translate, col_name, mapping_tables_dir, col_code = 1)
-                #if l.count('Unknown') > 0:
-                    #print(col_name)
+                col_code = 1
+                n_translated_values = translate_values(values_to_translate, col_name, mapping_tables_dir, col_code = col_code)
             else:
+                col_code = 0
                 n_translated_values = translated_values
 
+            # Append missing values not present in WR Report (due to cutoffs, etc)
+            translating_dict = create_mapping_dictionary(col_name, mapping_tables_dir, col_code = col_code)
 
-            if col_name == 'cod_variable_clinic':
-                if sheet_name == 'tb_variables':
-                    print(subset_df.iloc[:,0].values)
-                    print(translate_values(values_to_translate, col_name, mapping_tables_dir, col_code = 0))
-                    print(translated_values.count('Unknown'))
-                    print(len(translated_values))
-                    print(translate_values(values_to_translate, col_name, mapping_tables_dir, col_code = 1))
-                    print(out_dir + sheet_name + '__' + col_name + '.csv')
+            missing_values_wr = list(set(translating_dict.keys()) - set(values_to_translate))
+            missing_values_wr_df = pd.DataFrame(data = [[x,str(1)] for x in missing_values_wr],
+                                                columns = subset_df.columns)
+            subset_df = pd.concat([subset_df, missing_values_wr_df], axis = 0 )
+
+            n_translated_values += translate_values(missing_values_wr, col_name, mapping_tables_dir, col_code = col_code)
+
+            # Remove this once it's checked
+            # if col_name == 'cod_variable_clinic':
+            #    if sheet_name == 'tb_variables':
+            #        print(subset_df.iloc[:,0].values)
+            #        print(translate_values(values_to_translate, col_name, mapping_tables_dir, col_code = 0))
+            #        print(translated_values.count('Unknown'))
+            #        print(len(translated_values))
+            #        print(translate_values(values_to_translate, col_name, mapping_tables_dir, col_code = 1))
+            #        print(out_dir + sheet_name + '__' + col_name + '.csv')
             subset_df['codes'] = [x[0] for x in n_translated_values]
             subset_df['spanish description'] = [x[1] for x in n_translated_values]
-            subset_df['english description'] = [''] * len(n_translated_values)
+
+            if translate_descriptions:
+                translator= Translator(to_lang="en", from_lang="es")
+                translated_things = [translator.translate(x) for x in [x[1] for x in n_translated_values]]
+            else:
+                translated_things = [''] * len(subset_df['spanish description'].values)
+            subset_df['english description'] = translated_things
+
             subset_df['comment'] = [''] * len(n_translated_values)
 
 

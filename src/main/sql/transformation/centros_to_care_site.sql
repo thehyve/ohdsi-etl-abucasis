0857a84616d6b3d5977f5ddb7d6796b4a725b2fa
Maxim Moinat <maxim@thehyve.nl>
2018-10-22 17:47:35 +0200

    care sites with zona location

--- src/main/sql/transformation/centros_to_care_site.sql
+++ src/main/sql/transformation/centros_to_care_site.sql
@@ -1,29 +1,27 @@
-
-INSERT INTO care_site
+/**
+Care site name and location (zona). The place of service is not known.
+ */
+INSERT INTO cdm5.care_site
 (
-	care_site_id,
-	care_site_name,
-	place_of_service_concept_id,
-	location_id,
-	care_site_source_value,
-	place_of_service_source_value
+  care_site_id,
+  care_site_name,
+  location_id,
+  care_site_source_value,
+  place_of_service_concept_id
 )
-SELECT
-	tba_centros.cod_centro	AS	care_site_id,
+  SELECT
+    tba_centros.cod_centro :: INTEGER AS care_site_id,
 
-	tba_centros.des_centro	AS	care_site_name,
+    tba_centros.des_centro            AS care_site_name,
 
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	place_of_service_concept_id,
+    -- The zona that the centre is in
+    location.location_id              AS location_id,
 
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	location_id,
+    tba_centros.cod_centro            AS care_site_source_value,
 
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	care_site_source_value,
+    0                                 AS place_of_service_concept_id
 
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	place_of_service_source_value
-
-FROM tba_centros
+  FROM public.tba_centros
+    JOIN public.tba_centros_asignacion USING (cod_centro)
+    JOIN cdm5.location ON cod_zona = location_source_value
 ;

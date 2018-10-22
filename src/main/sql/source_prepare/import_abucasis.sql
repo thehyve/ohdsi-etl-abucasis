b35149fcd5934cbcb52db8614b69548a053f9386
Alberto Gil <alberto@thehyve.nl>
2018-10-22 17:50:25 +0200

    moved files from /sql dir to /src dir

---
+++ src/main/sql/source_prepare/import_abucasis.sql
@@ -0,0 +1,3 @@
+ \copy public.tb_sip_spo FROM /var/lib/postgresql/abucasis_fake_data/tb_sip_spo.csv DELIMITER ',' CSV HEADER ENCODING 'windows-1251';
+ \copy public.tb_sip_spo_resto_2012 FROM /var/lib/postgresql/abucasis_fake_data/tb_sip_spo_resto_2012.csv DELIMITER ',' CSV HEADER ENCODING 'windows-1251';
+ \copy public.tb_sip_spo_resto_2015 FROM /var/lib/postgresql/abucasis_fake_data/tb_sip_spo_resto_2015.csv DELIMITER ',' CSV HEADER ENCODING 'windows-1251';

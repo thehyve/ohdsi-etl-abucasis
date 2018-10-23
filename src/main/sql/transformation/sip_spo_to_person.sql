dc98f5f7e8a063314cb1af291900ec50cc3af9ba
Alberto Gil <alberto@thehyve.nl>
2018-10-23 13:50:43 +0200

    updated mapping rules. missing care site mapping

--- src/main/sql/transformation/sip_spo_to_person.sql
+++ src/main/sql/transformation/sip_spo_to_person.sql
@@ -10,11 +10,9 @@ INSERT INTO person
 	year_of_birth,
 	month_of_birth,
 	day_of_birth,
-	time_of_birth,
+	death_datetime,
 	race_concept_id, -- Unknown
 	ethnicity_concept_id, -- Unknown
-	location_id,
-	provider_id,
 	care_site_id,
 	gender_source_concept_id,
 	race_source_value,
@@ -23,57 +21,49 @@ INSERT INTO person
 	ethnicity_source_concept_id
 )
 SELECT
- -- [VALUE   COMMENT] Alphanumerical 
-	tb_sip_spo.numsipcod	AS	person_id,
+ -- [VALUE   COMMENT] Alphanumerical
+ -- TODO autoincrement
+	sip.numsipcod                                          AS	person_id,
 
- -- [VALUE   COMMENT] Alphanumerical 
-	tb_sip_spo.numsipcod	AS	person_source_value,
 
-	tb_sip_spo.sexo	AS	gender_concept_id,
+	sip.numsipcod                                          AS	person_source_value,
 
-	tb_sip_spo.sexo	AS	gender_source_value,
+  CASE sip.sexo
+  WHEN 'H'
+    THEN 8507 --MALE
+  WHEN 'M'
+    THEN 8532 --FEMALE
+  ELSE 0 -- Unknown
+  END                                                           AS	gender_concept_id,
 
- -- [VALUE   COMMENT] If 01-01, it is a proxy for birth year. 
-	tb_sip_spo.fecha_nac	AS	year_of_birth,
+	sip.sexo                                               AS	gender_source_value,
+	0                                                           	AS	gender_source_concept_id,
 
- -- [VALUE   COMMENT] If 01-01, it is a proxy for birth year. 
-	tb_sip_spo.fecha_nac	AS	month_of_birth,
+	YEAR(sip.fecha_nac)                                  	AS	year_of_birth,
 
- -- [VALUE   COMMENT] If 01-01, it is a proxy for birth year. 
-	tb_sip_spo.fecha_nac	AS	day_of_birth,
+	MONTH(sip.fecha_nac)                                 	AS	month_of_birth,
 
+	DAY(sip.fecha_nac)                                   	AS	day_of_birth,
+
+	0                                                           	AS	race_source_concept_id,
+	0                                                           	AS	race_concept_id,
+
+	0                                                           	AS	ethnicity_source_concept_id
+	0                                                           	AS	ethnicity_concept_id,
+
+
+  CASE
+  WHEN sipresto.fechadef IS NULL AND sipresto.fecha_baja_sip IS NULL THEN NULL
+  WHEN sipresto.fechadef IS NULL AND sipresto.fecha_baja_sip IS NOT NULL THEN sipresto.fechadef
+  WHEN sipresto.fechadef IS NOT NULL AND sipresto.fecha_baja_sip IS NULL THEN sipresto.fecha_baja_sip
+  END                                                            AS death_datetime,
  -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	time_of_birth,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	race_concept_id,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	ethnicity_concept_id,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	location_id,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	provider_id,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
+ --TODO
 	NULL	AS	care_site_id,
 
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	gender_source_concept_id,
 
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	race_source_value,
 
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	race_source_concept_id,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	ethnicity_source_value,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	ethnicity_source_concept_id
-
-FROM tb_sip_spo
+FROM public.tb_sip_spo sip
+  LEFT JOIN public.tb_sip_spo_resto_2015 sipresto
+  ON sip.numsipcod = sipresto.numsipcod
 ;

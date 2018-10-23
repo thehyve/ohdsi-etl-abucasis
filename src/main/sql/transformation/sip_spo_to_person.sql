5ab6f2aa519ecde7688928dcaf0b8ec45fd9fae1
Alberto Gil <alberto@thehyve.nl>
2018-10-23 15:02:44 +0200

    updated mapping rules

--- src/main/sql/transformation/sip_spo_to_person.sql
+++ src/main/sql/transformation/sip_spo_to_person.sql
@@ -21,44 +21,39 @@ INSERT INTO cdm5.person
 	ethnicity_source_concept_id
 )
 SELECT
- -- [VALUE   COMMENT] Alphanumerical
- -- TODO autoincrement
-	tb_sip_spo.numsipcod                                          AS	person_id,
+    tb_sip_spo.numsipcod                                          AS	person_source_value,
+
+    CASE tb_sip_spo.sexo
+    WHEN 'H'
+      THEN 8507 --MALE
+    WHEN 'M'
+      THEN 8532 --FEMALE
+    ELSE 0 -- Unknown
+    END                                                           AS	gender_concept_id,
+
+    tb_sip_spo.sexo                                               AS	gender_source_value,
+    0                                                           	AS	gender_source_concept_id,
+
+    YEAR(tb_sip_spo.fecha_nac)                                  	AS	year_of_birth,
+
+    MONTH(tb_sip_spo.fecha_nac)                                 	AS	month_of_birth,
+
+    DAY(tb_sip_spo.fecha_nac)                                   	AS	day_of_birth,
+
+    0                                                           	AS	race_source_concept_id,
+    0                                                           	AS	race_concept_id,
+
+    0                                                           	AS	ethnicity_source_concept_id
+    0                                                           	AS	ethnicity_concept_id,
 
 
-	tb_sip_spo.numsipcod                                          AS	person_source_value,
+    CASE
+    WHEN tb_sip_spo_resto_2015.fechadef IS NULL AND tb_sip_spo_resto_2015.fecha_baja_sip IS NOT NULL THEN sipresto.fechadef
+    WHEN tb_sip_spo_resto_2015.fechadef IS NOT NULL AND tb_sip_spo_resto_2015.fecha_baja_sip IS NULL THEN sipresto.fecha_baja_sip
+    ELSE NULL
+    END                                                            AS death_datetime,
 
-  CASE tb_sip_spo.sexo
-  WHEN 'H'
-    THEN 8507 --MALE
-  WHEN 'M'
-    THEN 8532 --FEMALE
-  ELSE 0 -- Unknown
-  END                                                           AS	gender_concept_id,
-
-	tb_sip_spo.sexo                                               AS	gender_source_value,
-	0                                                           	AS	gender_source_concept_id,
-
-	YEAR(tb_sip_spo.fecha_nac)                                  	AS	year_of_birth,
-
-	MONTH(tb_sip_spo.fecha_nac)                                 	AS	month_of_birth,
-
-	DAY(tb_sip_spo.fecha_nac)                                   	AS	day_of_birth,
-
-	0                                                           	AS	race_source_concept_id,
-	0                                                           	AS	race_concept_id,
-
-	0                                                           	AS	ethnicity_source_concept_id
-	0                                                           	AS	ethnicity_concept_id,
-
-
-  CASE
-  WHEN tb_sip_spo_resto_2015.fechadef IS NULL AND tb_sip_spo_resto_2015.fecha_baja_sip IS NOT NULL THEN sipresto.fechadef
-  WHEN tb_sip_spo_resto_2015.fechadef IS NOT NULL AND tb_sip_spo_resto_2015.fecha_baja_sip IS NULL THEN sipresto.fecha_baja_sip
-  ELSE NULL
-  END                                                            AS death_datetime,
-
-	care_site.care_site_id	                                      AS	care_site_id,
+    care_site.care_site_id	                                      AS	care_site_id,
 
 
 

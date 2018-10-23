a97ab9725501d26686f7ccc9401d0daa8bf5c903
Maxim Moinat <maxim@thehyve.nl>
2018-10-23 13:32:17 +0200

    socio economic and nationality to observation

---
+++ src/main/sql/transformation/nacionalidad_to_observation.sql
@@ -0,0 +1,47 @@
+/*
+Spanish nationality status as observation from the 2015 demographics.
+Value is either 'S', 'N' or 'D' (Yes, No, Unknown)
+*/
+INSERT INTO cdm5.observation
+(
+  person_id,
+  observation_concept_id,
+  observation_source_value,
+  observation_source_concept_id,
+  observation_date,
+  observation_datetime,
+  observation_type_concept_id,
+  value_as_concept_id
+)
+  SELECT
+    person.person_id                               AS person_id,
+
+    CASE tb_sip_spo_resto_2015.nacionalidad_espanola
+      -- Native
+      WHEN 'S' THEN 4135608
+      -- Migrant
+      WHEN 'N' THEN 4074479
+      -- Unknown
+      WHEN 'D' THEN 4185231
+      ELSE 0
+    END                                            AS observation_concept_id,
+
+    tb_sip_spo_resto_2015.nacionalidad_espanola    AS observation_source_value,
+
+    -- TODO: create custom 2B+ source concepts
+    0                                              AS observation_source_concept_id,
+
+    tb_sip_spo_resto_2015.fecha_corte              AS observation_date,
+
+    tb_sip_spo_resto_2015.fecha_corte :: TIMESTAMP AS observation_datetime,
+
+    -- Observation recorded from EHR
+    38000280                                       AS observation_type_concept_id,
+
+    -- Yes
+    4188539                                        AS value_as_concept_id
+
+  FROM public.tb_sip_spo_resto_2015
+    JOIN cdm5.person
+      ON person.person_source_value = tb_sip_spo_resto_2015.numsipcod
+;

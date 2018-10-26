8212500acaf79ad499cff277359a6040468b7144
Alberto Gil <alberto@thehyve.nl>
2018-10-26 11:20:08 +0200

    updated mapping. Now a UNIQUE VISIT_CONCEPT_ID is generated for each numsipcod/fecha_inicio present in ABUCASIS

--- src/main/sql/source_preprocessing/ante_cmbd_morbilid__visit_intermediate_table.sql
+++ src/main/sql/source_preprocessing/ante_cmbd_morbilid__visit_intermediate_table.sql
@@ -12,16 +12,21 @@ ALTER TABLE source_intermediate.intermediate_table_visit_ocurrence
 
 INSERT INTO source_intermediate.intermediate_table_visit_ocurrence (origin, numsipcod, date)
 
-SELECT 'C' AS origin,
-       tb_ante_cmbd.numsipcod AS numsipcod,
-       to_date(cast(tb_ante_cmbd.fecha_ingreso as TEXT),'YYYY-MM-DD') AS date
+SELECT 'C'                                                            AS origin,
+       unique_table.numsipcod                                         AS numsipcod,
+       TO_DATE(cast(unique_table.fecha_ingreso AS TEXT), 'YYYY-MM-DD')  AS date
+FROM
+     -- Retrieve unique combinations SIP - FECHA_INGRESO present in ABUCASIS
+     (SELECT DISTINCT numsipcod, fecha_ingreso FROM public.tb_ante_cmbd) AS unique_table
+;
 
-FROM public.tb_ante_cmbd;
 
 INSERT INTO source_intermediate.intermediate_table_visit_ocurrence (origin, numsipcod, date )
-SELECT 'M' AS origin,
-        tb_morbilid.numsipcod AS numsipcod,
-        TO_DATE(cast(tb_morbilid.fecha_inicio AS TEXT), 'YYYY-MM-DD')  AS date
-FROM public.tb_morbilid;
 
---TODO this table will need to be deleted after CDM population?
+SELECT 'M'                                                            AS origin,
+       unique_table.numsipcod                                         AS numsipcod,
+       TO_DATE(cast(unique_table.fecha_inicio AS TEXT), 'YYYY-MM-DD')  AS date
+FROM
+    -- Retrieve unique combinations SIP - FECHA_INICIO present in ABUCASIS
+     (SELECT DISTINCT numsipcod, fecha_inicio FROM public.tb_morbilid) AS unique_table
+;

e2b06b2e031f10aaf47e5595da3fb63cc730fd66
Maxim Moinat <maxim@thehyve.nl>
2018-10-26 18:55:37 +0200

    new stcm tables loading set up

--- src/main/sql/vocabulary_mapping/load_source_to_concept_map.sql
+++ src/main/sql/vocabulary_mapping/load_source_to_concept_map.sql
@@ -1,4 +1,7 @@
-/* Add the vocabulary ids */
+/* Remove existing source codes */
+TRUNCATE TABLE cdm5.source_to_concept_map
+;
+
 DELETE FROM cdm5.vocabulary
 WHERE vocabulary_concept_id = 0
 ;
@@ -6,11 +9,13 @@ WHERE vocabulary_concept_id = 0
 COPY cdm5.vocabulary FROM '@absPath/resources/mapping_tables/vocabulary.csv' WITH CSV HEADER
 ;
 
-/* Load new source to concept maps */
-TRUNCATE TABLE cdm5.source_to_concept_map
-;
-
 -- Mapping of name of table with info on number of events
 COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_num_events.csv' WITH CSV HEADER
+;
+COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_modalidad.csv' WITH CSV HEADER
+;
+COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_tip_variabl.csv' WITH CSV HEADER
+;
+COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_ud_medidas.csv' WITH CSV HEADER
 ;
 

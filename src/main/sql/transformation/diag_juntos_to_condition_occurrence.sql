e2d9016774ad073a5e6a65ecdb870618931b0c84
JolandaS <JolandaS@users.noreply.github.com>
2018-10-29 16:33:05 +0100

    Update_measurements_first_condition_occ
    Updated the sqls for measurement table and first version condition occ

--- src/main/sql/transformation/diag_juntos_to_condition_occurrence.sql
+++ src/main/sql/transformation/diag_juntos_to_condition_occurrence.sql
@@ -2,51 +2,38 @@
 Diagnoses (From hospitalization and ambulatory together
 Derived from tb_antepers and tb_ante_cmbd)
 */
-INSERT INTO condition_occurrence
+INSERT INTO cdm5.condition_occurrence
 (
-	condition_occurrence_id,
 	person_id,
 	condition_start_date,
 	condition_end_date,
 	condition_concept_id,
 	condition_source_value,
 	condition_type_concept_id,
-	stop_reason,
-	provider_id,
-	visit_occurrence_id,
 	condition_source_concept_id
 )
+;
 SELECT
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	condition_occurrence_id,
-
-	tb_diag_juntos.numsipcod	AS	person_id,
-
+ 	person.person_id AS person_id,
 	tb_diag_juntos.fecha_inicio	AS	condition_start_date,
-
+	tb_diag_juntos.fecha_inicio :: TIMESTAMP AS condition_start_datetime,
 	tb_diag_juntos.fecha_fin	AS	condition_end_date,
-
- -- [VALUE   COMMENT] ICD9CM and ICD9PROC  
-	tb_diag_juntos.cod_diagnostico	AS	condition_concept_id,
-
- -- [VALUE   COMMENT] ICD9CM and ICD9PROC  
+	tb_diag_juntos.fecha_fin :: TIMESTAMP AS condition_end_datetime,
+ -- [VALUE   COMMENT] ICD9CM and ICD9PROC
+	--icd9cm.concept_code AS condition_concept_id,
+ -- [VALUE   COMMENT] ICD9CM and ICD9PROC
 	tb_diag_juntos.cod_diagnostico	AS	condition_source_value,
-
- -- [VALUE   COMMENT] empty = ambulatory care 1 = primary diagnosis in hospital 
- -- [MAPPING   LOGIC] 1 or empty - 44786627 Primary condition others - 38000245 problem list entry 
 	tb_diag_juntos.orden	AS	condition_type_concept_id,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	stop_reason,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
-	NULL	AS	provider_id,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
 	NULL	AS	visit_occurrence_id,
-
- -- [!WARNING!] no source column found. See possible comment at the INSERT INTO
 	NULL	AS	condition_source_concept_id
-
 FROM tb_diag_juntos
+JOIN cdm5.person
+      ON numsipcod = person.person_source_value
+--LEFT JOIN cdm5.concept AS icd9cm
+--      ON icd9cm.concept_code = tb_diag_juntos.cod_diagnostico AND icd9cm.vocabulary_id = 'ICD9CM'
+--    LEFT JOIN cdm5.concept_relationship AS code_map
+--     ON code_map.concept_id_1 = icd9cm.concept_id
+--         AND code_map.relationship_id = 'Maps to'
+WHERE origen in ('C','M')
+--TODO: link to ICD9 codes, correct and complete the select statement
 ;
--- src/main/sql/transformation/prestaci_to_measurement.sql
+++ src/main/sql/transformation/prestaci_to_measurement.sql
@@ -3,34 +3,34 @@ Clinical measurements
 */
 INSERT INTO cdm5.measurement
 (
-	measurement_id,
 	person_id,
 	value_as_number,
 	measurement_concept_id,
 	measurement_source_concept_id,
 	measurement_source_value,
 	measurement_date,
+	measurement_datetime,
 	unit_concept_id,
 	unit_source_value,
-	measurement_time,
 	measurement_type_concept_id
 )
 ;
 SELECT
- 	NULL	AS	measurement_id,
-	person.person_id AS person_id,
+ 	person.person_id AS person_id,
 	valor_registrado AS value_as_number,
- 	cod_prestacion	AS measurement_concept_id,
- 	cod_prestacion	AS measurement_source_concept_id,
+ 	coalesce(pres.target_concept_id,0)AS measurement_concept_id,
+ 	coalesce(pres.source_concept_id,0)	AS measurement_source_concept_id,
  	cod_prestacion AS measurement_source_value,
 	fecha_registro	AS	measurement_date, --TODO: check date type
 	fecha_registro :: TIMESTAMP AS measurement_datetime,
- 	cod_ud_medida AS unit_concept_id,
- 	cod_ud_medida	AS	unit_source_value,
+ 	coalesce(unit.target_concept_id,0) AS unit_concept_id,
+ 	coalesce(unit.source_concept_id)	AS	unit_source_value,
   44818702 AS measurement_type_concept_id
 FROM public.tb_prestaci
-  LEFT JOIN cdm5.person ON numsipcod = person.person_source_value
-  --TODO: link to concept_id table on cod_prestacion
-  --TODO: link to source_conept_mapping on cod_ud_medida
-WHERE cod_prestacion NOT LIKE '-1'
+  JOIN cdm5.person ON numsipcod = person.person_source_value
+  --only patients also present in person table to be included
+  --TODO: add abucasis_prestacion concepts to source_to_concept_map table
+  LEFT JOIN cdm5.source_to_concept_map pres ON cod_prestacion = pres.source_code AND pres.source_vocabulary_id = 'ABUCASIS_TIP_PREST'
+  LEFT JOIN cdm5.source_to_concept_map unit ON cod_ud_medida = unit.source_code AND unit.source_vocabulary_id = 'ABUCASIS_UD_MEDIDAS'
+WHERE public.tb_prestaci.cod_prestacion NOT LIKE '-1'
 ;

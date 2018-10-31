34de2ffd7f481542ad8f120ac7e85650e1e608b7
Alberto Gil <alberto@thehyve.nl>
2018-10-31 16:03:50 +0100

    added comments to death generation table. This table is only created to make the CDM backwards compatible with CDM v5

--- src/main/sql/transformation/sip_spo_to_death.sql
+++ src/main/sql/transformation/sip_spo_to_death.sql
@@ -1,5 +1,5 @@
 /*
-
+This table is NOT part of CDM v6 BUT it has been implemented to make the CDM backwards compatible with CDM v5.
  */
 INSERT INTO cdm5.death
 (
@@ -13,7 +13,9 @@ SELECT
   person_id AS person_id,
   observation_date AS death_date,
   observation_datetime  AS death_datetime,
+  -- EHR discharge status ‘Expired’ OR Observation recorded from EHR
   observation_type_concept_id AS death_type_concept_id
+
 FROM cdm5.observation
 WHERE observation_concept_id = 4306655
 --select from observation table only the death occurences

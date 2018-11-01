5a4617f20c07d9a9abee13f2f271e14a8650218b
Alberto Gil <alberto@thehyve.nl>
2018-11-01 10:22:54 +0100

    added new unit tests

---
+++ src/test/end_to_end/README.md
@@ -0,0 +1,22 @@
+# Unit tests for the CALIBER ETL
+
+## Dependencies
+yaml package (R):
+```
+install.packages('yaml')
+```
+
+## Setup
+Copy `config.example.yml` and rename to `config.yml`. Enter your connection details.
+
+The `sourceSchema` is the schema where the mock data will be inserted. 
+Create this schema and the (empty) source tables separately (e.g. with the White Rabbit Fake data generator).
+
+## Usage
+Run `main.R` in RStudio up to the test data insertion.
+Then, run your ETL on the test data.
+Finally, run the last part of `main.R` to execute the tests.
+
+## Utilities
+cdm5test
+abucasis_test
--- src/test/end_to_end/main.R
+++ src/test/end_to_end/main.R
@@ -1,5 +1,7 @@
 library('yaml')
 source('TestFrameworkAbucasis.R') # Note: this is the pre-factorization (Dec-2017) framework
+#source('src/test/end_to_end/TestFrameworkAbucasis.R') # Note: this is the pre-factorization (Dec-2017) framework
+
 
 # get_defaults_all_clinic()
 
@@ -57,6 +59,32 @@ add_tb_proc_cmbd(numsipcod = 'A02', fecha_ingreso = '2018-01-01', tipo_actividad
 expect_procedure_occurrence(procedure_source_value = '20.20', visit_occurrence_id = 1) # NOTE: this id can change
 
 
+
+# ========================
+# Observation
+# ========================
+declareTest('Observation death if patient died during hospitalization')
+add_tb_sip_spo(numsipcod='A06', fecha_nac='1991-01-01', fecha_def='2012-02-05')
+add_tb_ante_cmbd(numsipcod='A06', cir_alta='10', fecha_ingreso = '2012-01-01', fecha_alta = '2012-02-05')
+expect_observation(person_id = 6, observation_type_concept_id = 4216643)
+
+declareTest('Observation death if patient died NOT during hospitalization')
+add_tb_sip_spo(numsipcod='A07', fecha_nac='1994-01-01', fecha_def='2016-02-05')
+expect_observation(person_id = 7, observation_type_concept_id = 38000280)
+
+# ========================
+# Measurement
+# ========================
+declareTest('NO Measurement if cod_ud_medida has conflictive value')
+add_tb_prestaci(numsipcod='A06', cod_ud_medida = ".")
+expect_no_measurement(person_id = 6)
+add_tb_prestaci(numsipcod='A05', cod_ud_medida = "I")
+expect_no_measurement(person_id = 5)
+add_tb_variables(numsipcod='A04', cod_ud_medida = ".")
+expect_no_measurement(person_id = 4)
+add_tb_variables(numsipcod='A03', cod_ud_medida = "I")
+expect_no_measurement(person_id = 3)
+
 # ========================
 # ========================
 #
@@ -87,14 +115,16 @@ executeSql(connection, paste(insert_sql, collapse = '\n'))
 # ETL time!
 #
 # Run etl script with config$sourceSchema as source:
+# python3 main.py -p 6000 -d abucasis_dev -u postgres -w postgres -s abucasis_test -c cdm5test --skipvocab
+
+system(sprintf('python3 ../../../main.py -p %s -d abucasis_dev -u %s -w %s -s %s --skipvocab', connectionConfig$port, connectionConfig$user, connectionConfig$password, config$sourceSchema, config$cdmSchema))
+# ========================
+# ========================
 #
-# ========================
-# ========================
-
-test_sql <- testSql
-executeSql(connection, sprintf('SET search_path TO %s;', config$cdmSchema))
-test_sql[1] <- 'DROP TABLE IF EXISTS test_results;' # Replace existing SQL server specific table drop
-executeSql(connection, paste(test_sql, collapse = ';\n'))
-
-# View the results
-querySql(connection, 'SELECT * FROM test_results')
+# test_sql <- testSql
+# executeSql(connection, sprintf('SET search_path TO %s;', config$cdmSchema))
+# test_sql[1] <- 'DROP TABLE IF EXISTS test_results;' # Replace existing SQL server specific table drop
+# executeSql(connection, paste(test_sql, collapse = ';\n'))
+#
+# # View the results
+# querySql(connection, 'SELECT * FROM test_results')

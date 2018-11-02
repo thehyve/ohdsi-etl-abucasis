9b20735ad2f163fb7e8e04bfc27d0feb888062c4
Alberto Gil <alberto@thehyve.nl>
2018-11-02 18:02:42 +0100

    added script for creating additional columns to drug_era and condition_occurrence tables to make Achilles compatible with CDM v6

---
+++ src/test/achilles_run/init_achilles.sql
@@ -0,0 +1,22 @@
+-- Create columns
+
+-- ALTER TABLE cdm5.drug_era
+-- ADD COLUMMN drug_era_start_date date
+-- ALTER TABLE cdm5.drug_era
+--   ADD COLUMMN drug_era_end_date date
+-- ALTER TABLE cdm5.condition_era
+--     ADD COLUMMN condition_era_start_date date
+-- ALTER TABLE cdm5.condition_era
+--   ADD COLUMMN condition_era_end_date date;
+
+UPDATE cdm5.drug_era
+SET drug_era_start_date = drug_era_start_datetime;
+UPDATE cdm5.drug_era
+SET drug_era_end_date = drug_era_end_datetime;
+
+UPDATE cdm5.condition_era
+SET condition_era_start_date = condition_era_start_datetime;
+
+UPDATE cdm5.condition_era
+SET condition_era_end_date = condition_era_end_datetime;
+

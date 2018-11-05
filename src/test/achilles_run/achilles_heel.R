a805932334b18b123c573f5c79aa04572249f26a
Alberto Gil <alberto@thehyve.nl>
2018-11-05 14:50:38 +0100

    implemented automatic pre- and post-processing of CDM to make the CDMv6 compatible with latest version of Achilles (which assumes CDMv5)

--- src/test/achilles_run/achilles_heel.R
+++ src/test/achilles_run/achilles_heel.R
@@ -1,30 +1,36 @@
+### Install required dependencies  ###
+if (!require("devtools")) install.packages("devtools")
+# Install Achilles latest release
+if (!require("Achilles")) devtools::install_github("OHDSI/Achilles", dependencies = TRUE)
+# # Install Achilles for CDM v6 (current state: does not work)
+# devtools::install_github("OHDSI/Achilles", ref="cdm_v6", dependencies = TRUE)
+
 library(Achilles)
 library('yaml')
 library(DatabaseConnector)
 
-# setwd("ohdsi-etl-abucasis/src/test/achilles_run/")
+# TODO set working directories, etc 
 
+# Load database connection details, schema names, CDM version, ...
 config <- yaml.load_file('./config.yml')
-# connectionDetails <- createConnectionDetails(dbms = "postgresql",
-#                                              user = "postgres",
-#                                              password = "postgres",
-#                                              server = "localhost/abucasis_dev",
-#                                              port = "6000",
-#                                              schema="cdm5")
 
-#
+# Connect to the database 
 connectionConfig <- config$connectionDetails
 connectionDetails <- createConnectionDetails(dbms = "postgresql",
                                              user = connectionConfig$user,
                                              password = connectionConfig$password,
                                              server = connectionConfig$server,
                                              port = connectionConfig$port)
-# connection <- connect(connectionDetails)
-#
-#
-# TODO runn achilles heel
-# do not check statistical
-# which analysis we want to run, ...
+
+############ CDM pre-processing ############ 
+# Temporary solution to solve version incompatibilities with latest release Achilles
+# Generate drug_era_start_date condition_era_start_date and end_date columns
+connection <- connect(connectionDetails)
+sqlquery <- paste(unlist(read.csv("./init_achilles.sql"), use.names=FALSE), collapse='\n')
+executeSql(connection, sqlquery)
+######################## 
+
+# Run Achilles
 achilles(connectionDetails,
          cdmDatabaseSchema = config$cdmSchema,
          resultsDatabaseSchema=config$cdmresultsSchema,
@@ -32,17 +38,42 @@ achilles(connectionDetails,
          scratchDatabaseSchema = config$cdmscratchSchema,
          numThreads = 1,
          sourceName = config$sourceName,
-         cdmVersion = 6,
+         cdmVersion = config$cdmversion,
          runHeel = TRUE,
          runCostAnalysis = FALSE,
          dropScratchTables = TRUE,
          conceptHierarchy = FALSE)
 
+### Run Achilles heel
+# achillesHeel(connectionDetails,
+#             cdmDatabaseSchema = config$cdmSchema,
+#             resultsDatabaseSchema=config$cdmresultsSchema,
+#             vocabDatabaseSchema = config$cdmSchema,
+#             scratchDatabaseSchema = config$cdmscratchSchema,
+#             numThreads = 1,
+#             outputFolder = ".",
+#             cdmVersion = config$cdmversion)
 
-exportToJson(connectionDetails,
-            cdmDatabaseSchema = config$cdmSchema,
-            resultsDatabaseSchema = config$cdmresultsSchema,
-            outputPath = getwd(),
-            vocabDatabaseSchema = config$cdmSchema,
-            compressIntoOneFile = TRUE # creates gzipped file of all JSON files
-            )
+## Export results to Json
+# exportToJson(connectionDetails,
+#             cdmDatabaseSchema = config$cdmSchema,
+#             resultsDatabaseSchema = config$cdmresultsSchema,
+#             outputPath = getwd(),
+#             vocabDatabaseSchema = config$cdmSchema,
+#             compressIntoOneFile = TRUE # creates gzipped file of all JSON files
+#             )
+
+############ CDM post-processing ############ 
+# Remove drug_era_start_date condition_era_start_date and end_date columns
+sqlquery <- paste(unlist(read.csv("./end_achilles.sql"), use.names=FALSE), collapse='\n')
+executeSql(connection, sqlquery)
+######################## 
+
+# Export AchillesHeel results 
+# fetchAchillesHeelResults(connectionDetails,config$cdmresultsSchema)
+launchHeelResultsViewer(connectionDetails,
+                        resultsDatabaseSchema = config$cdmresultsSchema,
+                        cdmDatabaseSchema = config$cdmSchema,
+                        outputFolder = ".")
+
+# getAnalysisDetails()

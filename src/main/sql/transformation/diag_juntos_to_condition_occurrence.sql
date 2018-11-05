dd2f49d064c2da1d63a6128c5526f0f9751eaae9
Maxim Moinat <maxim@thehyve.nl>
2018-11-05 11:15:05 +0100

    schema set to @source_schema and mapping cie9 from stcm

--- src/main/sql/transformation/diag_juntos_to_condition_occurrence.sql
+++ src/main/sql/transformation/diag_juntos_to_condition_occurrence.sql
@@ -4,49 +4,47 @@ Derived from tb_antepers and tb_ante_cmbd)
 */
 INSERT INTO cdm5.condition_occurrence
 (
-	person_id,
-	condition_start_date,
-	condition_start_datetime,
-	condition_end_date,
-	condition_end_datetime,
-	condition_concept_id,
-	condition_source_value,
-	condition_source_concept_id,
-	condition_type_concept_id,
-	condition_status_concept_id,
-	visit_occurrence_id
+  person_id,
+  condition_start_date,
+  condition_start_datetime,
+  condition_end_date,
+  condition_end_datetime,
+  condition_concept_id,
+  condition_source_concept_id,
+  condition_source_value,
+  condition_type_concept_id,
+  condition_status_concept_id,
+  visit_occurrence_id
 )
+  SELECT
+    person.person_id                                      AS person_id,
+    tb_diag_juntos.fecha_inicio                           AS condition_start_date,
+    tb_diag_juntos.fecha_inicio :: TIMESTAMP              AS condition_start_datetime,
+    tb_diag_juntos.fecha_fin                              AS condition_end_date,
+    tb_diag_juntos.fecha_fin :: TIMESTAMP                 AS condition_end_datetime,
+    coalesce(icd_map.target_concept_id, 0)                AS condition_concept_id,
+    coalesce(icd_map.source_concept_id, 0)                AS condition_source_concept_id,
+    tb_diag_juntos.cod_diagnostico                        AS condition_source_value,
+    CASE WHEN tb_diag_juntos.orden = 1
+              OR tb_diag_juntos.orden IS NULL
+      THEN 44786630 -- primary condition
+      ELSE 44786631 -- secondary condition
+    END                                                   AS condition_type_concept_id,
+    -- Clinical diagnosis
+    4309119                                               AS condition_status_concept_id,
+    intermediate_table_visit_ocurrence.visit_ocurrence_id AS visit_occurrence_id
 
-SELECT
- 	person.person_id AS person_id,
-	tb_diag_juntos.fecha_inicio	AS condition_start_date,
-	tb_diag_juntos.fecha_inicio :: TIMESTAMP AS condition_start_datetime,
-	tb_diag_juntos.fecha_fin	AS condition_end_date,
-	tb_diag_juntos.fecha_fin :: TIMESTAMP AS condition_end_datetime,
-  coalesce(icd9cm.concept_id, 0) AS condition_concept_id,
- 	tb_diag_juntos.cod_diagnostico AS condition_source_value,
- 	coalesce(code_map.concept_id_1, 0) AS condition_source_concept_id,
-	CASE WHEN tb_diag_juntos.orden= 1 OR tb_diag_juntos.orden IS NULL
-	    -- primary condition
-      THEN 44786630
-      -- secondary condition
-      ELSE 44786631 END AS condition_type_concept_id,
-  4309119 AS condition_status_concept_id, -- Clinical diagnosis
-	intermediate_table_visit_ocurrence.visit_ocurrence_id AS visit_occurrence_id
-
-FROM tb_diag_juntos
---link to visit_occurence_id on patient id, start date and origin of data via intermediate table
-LEFT JOIN source_intermediate.intermediate_table_visit_ocurrence
-         ON (tb_diag_juntos.numsipcod = intermediate_table_visit_ocurrence.numsipcod
-               AND
-             tb_diag_juntos.fecha_inicio = intermediate_table_visit_ocurrence.date
-              AND
-              tb_diag_juntos.origen = intermediate_table_visit_ocurrence.origin)
-INNER JOIN cdm5.person
+  FROM @source_schema.tb_diag_juntos
+    --link to visit_occurence_id on patient id, start date and origin of data via intermediate table
+    LEFT JOIN source_intermediate.intermediate_table_visit_ocurrence
+      ON (tb_diag_juntos.numsipcod = intermediate_table_visit_ocurrence.numsipcod
+          AND
+          tb_diag_juntos.fecha_inicio = intermediate_table_visit_ocurrence.date
+          AND
+          tb_diag_juntos.origen = intermediate_table_visit_ocurrence.origin)
+    INNER JOIN cdm5.person
       ON tb_diag_juntos.numsipcod = person.person_source_value
-LEFT JOIN cdm5.concept AS icd9cm
-      ON icd9cm.concept_code = tb_diag_juntos.cod_diagnostico AND icd9cm.vocabulary_id in ('ICD9CM', 'ICD10CM')
-    LEFT JOIN cdm5.concept_relationship AS code_map
-     ON code_map.concept_id_1 = icd9cm.concept_id AND code_map.relationship_id = 'Maps to'
-WHERE origen in ('C','M')
+    LEFT JOIN cdm5.source_to_concept_map AS icd_map
+      ON tb_diag_juntos.cod_diagnostico = icd_map.source_code AND icd_map.source_vocabulary_id = 'ABUCASIS_CIE9'
+  WHERE origen IN ('C', 'M')
 ;

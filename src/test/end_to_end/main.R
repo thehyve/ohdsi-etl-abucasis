f5c258a1c6a5fe167f91fe47f3d2994614629d77
Alberto Gil <alberto@thehyve.nl>
2018-11-20 11:46:26 +0100

    added test for checking if a condition occurrence is linked to a valid visit_id if the condition is present in tb_diag_juntos but not in tb_ante_cmbd

--- src/test/end_to_end/main.R
+++ src/test/end_to_end/main.R
@@ -114,6 +114,7 @@ add_tb_morbilid(numsipcod='A02', fecha_inicio = '2014-01-01', fecha_fin = '2013-
 expect_visit_occurrence(person_id = 2, visit_start_date = '2014-01-01', visit_end_date = '2014-01-01')
 
 
+
 # ========================
 # Condition occurrence
 # ========================
@@ -122,6 +123,12 @@ expect_person(person_id=2, person_source_value='A02')
 add_tb_ante_cmbd(numsipcod='A02', fecha_ingreso='2013-01-01', fecha_alta='2013-02-01')
 add_tb_diag_juntos(numsipcod='A02', fecha_inicio='2013-01-01', fecha_fin='2013-02-01', origen = 'C')
 expect_condition_occurrence(person_id = 2, condition_start_date = '2013-01-01', condition_end_date = '2013-02-01')
+
+
+declareTest('Condition occurrence from tb_diag_juntos and not tb_ante_cmbd linked to a valid visit_id')
+add_tb_diag_juntos(numsipcod='A02', fecha_inicio = '2012-08-01', fecha_fin = '2013-03-01')
+expect_condition_occurrence(person_id = 2, visit_occurrence_id = 7)
+
 
 # ========================
 # Drug exposure and era

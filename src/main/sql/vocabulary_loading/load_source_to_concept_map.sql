6d52f6eccf88d08b0511e6ceb04ecf870e2bf951
Alberto Gil <alberto@thehyve.nl>
2018-11-29 15:33:41 +0100

    retrieve only values if target_vocabulary_id is not null. \n solves a conflict with null target_vocabulary_id when target_concept_id=0

--- src/main/sql/vocabulary_loading/load_source_to_concept_map.sql
+++ src/main/sql/vocabulary_loading/load_source_to_concept_map.sql
@@ -14,20 +14,35 @@ INSERT INTO cdm5.vocabulary
   SELECT DISTINCT ON (vocabulary_id) *
   FROM tmp_vocabulary
 ON CONFLICT DO NOTHING;
-
-
+--
+--
 -- Mapping of name of table with info on number of events
-COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_num_events.csv' WITH CSV HEADER
+
+CREATE TEMP TABLE tmp_source_to_concept_map
+  AS
+    SELECT *
+    FROM cdm5.source_to_concept_map
+WITH NO DATA;
+
+
+
+COPY tmp_source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_num_events.csv' WITH CSV HEADER
 ;
-COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_modalidad.csv' WITH CSV HEADER
+COPY tmp_source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_modalidad.csv' WITH CSV HEADER
 ;
-COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_tip_variabl.csv' WITH CSV HEADER
+COPY tmp_source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_tip_variabl.csv' WITH CSV HEADER
 ;
-COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_ud_medidas.csv' WITH CSV HEADER
+COPY tmp_source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_ud_medidas.csv' WITH CSV HEADER
 ;
-COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_tip_prest.csv' WITH CSV HEADER
+COPY tmp_source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_tip_prest.csv' WITH CSV HEADER
 ;
-COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_prinactivo.csv' WITH CSV HEADER
+COPY tmp_source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_prinactivo.csv' WITH CSV HEADER
 ;
-COPY cdm5.source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_cie9.csv' WITH CSV HEADER
+COPY tmp_source_to_concept_map FROM '@absPath/resources/mapping_tables/abucasis_cie9.csv' WITH CSV HEADER
 ;
+
+INSERT INTO cdm5.source_to_concept_map
+  SELECT *
+  FROM tmp_source_to_concept_map
+  WHERE target_vocabulary_id IS NOT NULL
+ON CONFLICT DO NOTHING;

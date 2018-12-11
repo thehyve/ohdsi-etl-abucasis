4a202d53607e86102268752b905777bd88cdb8f5
Alberto Gil <alberto@thehyve.nl>
2018-12-11 17:25:34 +0100

    commented ante_cmbd_morbilid to intermediate visit table transformation script

--- src/main/sql/source_preprocessing/ante_cmbd_morbilid__visit_intermediate_table.sql
+++ src/main/sql/source_preprocessing/ante_cmbd_morbilid__visit_intermediate_table.sql
@@ -19,6 +19,10 @@ CREATE UNIQUE INDEX intermediate_table_visit_ocurrence_numsipcod_date_origin_uin
   ON source_intermediate.intermediate_table_visit_ocurrence (numsipcod, date, origin)
 ;
 
+/*
+Retrieve all unique combinations numsip-date-origin and create a visit
+from source table ante_cmbd, morbilid, diag_juntos
+*/
 INSERT INTO source_intermediate.intermediate_table_visit_ocurrence (origin, numsipcod, date)
   SELECT *
   FROM (

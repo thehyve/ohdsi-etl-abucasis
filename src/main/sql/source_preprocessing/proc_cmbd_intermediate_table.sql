1c54961358151e62b7dc9c5dcf737b4f6f194c21
Alberto Gil <alberto@thehyve.nl>
2018-12-12 15:51:46 +0100

    fix on intermediate procedure table derivation. Target vocabulary id is not retrieved from the target concept, rather than from the source concept (error fixed).

--- src/main/sql/source_preprocessing/proc_cmbd_intermediate_table.sql
+++ src/main/sql/source_preprocessing/proc_cmbd_intermediate_table.sql
@@ -34,18 +34,18 @@ SELECT
 
       intermediate_table_visit_ocurrence.visit_ocurrence_id        AS visit_occurrence_id,
 
-      icd9proc.domain_id                      AS target_domain_id
+      snomedcode.domain_id                      AS target_domain_id
 
 INTO source_intermediate.intermediate_proc_cmbd
 
 FROM  @source_schema.tb_proc_cmbd
-  --JOIN cdm5.person
-  --  ON person.person_source_value = tb_proc_cmbd.numsipcod
   LEFT JOIN cdm5.concept AS icd9proc
     ON icd9proc.concept_code = tb_proc_cmbd.cie9p AND icd9proc.vocabulary_id IN ('ICD9Proc', 'ICD10PCS')
   LEFT JOIN cdm5.concept_relationship AS code_map
     ON code_map.concept_id_1 = icd9proc.concept_id
        AND code_map.relationship_id = 'Maps to'
+  LEFT JOIN cdm5.concept AS snomedcode
+    ON snomedcode.concept_id = code_map.concept_id_2
   LEFT JOIN source_intermediate.intermediate_table_visit_ocurrence
     ON tb_proc_cmbd.numsipcod = intermediate_table_visit_ocurrence.numsipcod
        AND tb_proc_cmbd.fecha_ingreso = intermediate_table_visit_ocurrence.date

57d0145e65cbf523b943206b7c03ca6d26379796
Alberto Gil <alberto@thehyve.nl>
2018-12-17 16:44:59 +0100

    modified remove_conflictive_visits.sql. Now it first deletes the duplicated visits consequence of a discharge to another hospital and keeps only the longest visit. Then, it identifies and drops rows from visit_occurrence that have duplicated visit_occurrence_id primary keys.

--- src/main/sql/transformation/remove_conflictive_visits.sql
+++ src/main/sql/transformation/remove_conflictive_visits.sql
@@ -1,43 +1,57 @@
 -- Remove conflictive visits
 
 ------------------ Rule 1 --------------------
--- look for multiple visit ocurrences for patients that were transfered to another hospital
+-- look for multiple visit occurrences for patients that were transferred to another hospital
 -- (discharge_to_concept_id = 8717) with duplicated admission dates (they were not properly merged in ABUCASIS)
-
 -- (the duplicated visit with the shortest discharge to concept id will be removed)
-WITH cte1 AS (
+
+WITH cte0 AS (
+    -- Generate table with count of visit_occurrence_id
+    -- will be used to identify duplicated visit_occurrence_id in visit_occurrence_table
+    SELECT visit_occurrence_id AS visitid, COUNT(*)
+    FROM cdm5.visit_occurrence
+    GROUP BY visit_occurrence_id
+    ),
+     cte1 AS (
+     -- Identify patients that were transfered to another hospital
     DELETE
         FROM cdm5.visit_occurrence
         WHERE discharge_to_concept_id = 8844 AND visit_source_value != 'tb_morbilid'
-        returning *
-    ), cte2 AS (
+     returning *
+    ),
+     cte2 AS (
+     -- Sort rows by increasing start and end date
+     -- will be used to capture in OMOP only the visit with the longest time span
     SELECT
            row_number() over(partition by visit_occurrence_id, visit_start_date ORDER BY visit_occurrence_id, visit_start_date, visit_end_date DESC) AS row_number,
            *
     FROM cte1
     )
 INSERT INTO cdm5.visit_occurrence
-    SELECT
-        visit_occurrence_id		,
-        person_id					 ,
-        visit_concept_id				 ,
-        visit_start_date		,
-        visit_start_datetime		 ,
-        visit_end_date			 ,
-        visit_end_datetime			 ,
-        visit_type_concept_id			 ,
-        provider_id					,
-        care_site_id					,
-        visit_source_value			,
-        visit_source_concept_id		 ,
-        admitted_from_concept_id       ,
-        admitted_from_source_value     ,
-        discharge_to_source_value	 ,
-        discharge_to_concept_id		,
-        preceding_visit_occurrence_id
+SELECT
+       visit_occurrence_id		,
+       person_id					 ,
+       visit_concept_id				 ,
+       visit_start_date		,
+       visit_start_datetime		 ,
+       visit_end_date			 ,
+       visit_end_datetime			 ,
+       visit_type_concept_id			 ,
+       provider_id					,
+       care_site_id					,
+       visit_source_value			,
+       visit_source_concept_id		 ,
+       admitted_from_concept_id       ,
+       admitted_from_source_value     ,
+       discharge_to_source_value	 ,
+       discharge_to_concept_id		,
+       preceding_visit_occurrence_id
 
-    FROM cte2
-    WHERE row_number != 1
+FROM cte2
+LEFT JOIN cte0
+ON cte2.visit_occurrence_id = cte0.visitid
+-- Insert the longest visit (row number > 1) or if there's a unique record for that visit_occurrence_id capture it in the table
+WHERE row_number != 1 OR (row_number=1 AND cte0.count = 1)
 ;
 
 --------------------- Rule 2 -------------------
@@ -77,7 +91,6 @@ INSERT INTO cdm5.visit_occurrence
         discharge_to_source_value	 ,
         discharge_to_concept_id		,
         preceding_visit_occurrence_id
-
     FROM cte2
     WHERE row_number = 1
 ;

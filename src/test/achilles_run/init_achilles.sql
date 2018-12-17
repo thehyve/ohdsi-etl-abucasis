2db2c771a1ca4b847fd00b896b0f359955a70f02
Alberto Gil <alberto@thehyve.nl>
2018-12-17 16:46:20 +0100

    check if column exists before creating it in achilles initialization script init_achilles.sql

--- src/test/achilles_run/init_achilles.sql
+++ src/test/achilles_run/init_achilles.sql
@@ -1,17 +1,16 @@
 -- Create columns
 --Drug era
 ALTER TABLE cdm5.drug_era
-  ADD COLUMN drug_era_start_date date;
+  ADD COLUMN IF NOT EXISTS drug_era_start_date date;
 ALTER TABLE cdm5.drug_era
-  ADD COLUMN drug_era_end_date date;
+  ADD COLUMN IF NOT EXISTS drug_era_end_date date;
 -- Condition era
 ALTER TABLE cdm5.condition_era
-    ADD COLUMN condition_era_start_date date;
+    ADD COLUMN IF NOT EXISTS condition_era_start_date date;
 ALTER TABLE cdm5.condition_era
-  ADD COLUMN condition_era_end_date date;
+  ADD COLUMN IF NOT EXISTS condition_era_end_date date;
 
--- Copy data from <>_start_datetime column to <>_start_date column (also for end_date)
-
+---- Copy data from <>_start_datetime column to <>_start_date column (also for end_date)
 -- Drug era
 UPDATE cdm5.drug_era
 SET drug_era_start_date = drug_era_start_datetime;

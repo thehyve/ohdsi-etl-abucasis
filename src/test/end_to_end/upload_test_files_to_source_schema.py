eee7604b81f40f1b5f756f06d22d696b18d02e6b
egarcialara <e.oohla@hotmail.com>
2021-07-20 17:05:17 +0200

    small change

--- src/test/end_to_end/upload_test_files_to_source_schema.py
+++ src/test/end_to_end/upload_test_files_to_source_schema.py
@@ -18,6 +18,7 @@ def upload_tables():
     for t in tables:
         t_name = t[(len(tables_dir)):-4]
         t_name2 = t_name.lower()
+        if t_name2 in ['initframework', 'generateinsertsq', 'summary.frameworkContext', 'summarytestframework', 'declaretest', 'getuntestedsourcefields', 'expect_person', 'config', 'gettestsoverview']: continue
         cmd_line_create = f"sudo -u postgres psql -d ETL_UKBiobank -c \"create table {schema}.{t_name2} (like abucasis_original.{t_name2} including all); \" "
         print(f"Running:\n{cmd_line_create}")
         os.system(cmd_line_create)
@@ -26,6 +27,7 @@ def upload_tables():
     for t in tables:
         t_name = t[(len(tables_dir)):-4]
         t_name2 = t_name.lower()
+        if t_name2 in ['initframework', 'generateinsertsq', 'summary.frameworkContext', 'summarytestframework', 'declaretest', 'getuntestedsourcefields', 'expect_person', 'config', 'gettestsoverview']: continue
         cmd_line = f"sudo -u postgres psql -d ETL_UKBiobank -c \"\copy {schema}.{t_name2} FROM '{t}' delimiter ',' csv header  \" "
         print(f"Running:\n{cmd_line}")
         os.system(cmd_line)

c6fed07e5af308c9e631660648efa9204aa1ec65
egarcialara <e.oohla@hotmail.com>
2021-07-29 14:02:32 +0200

    add new generate drug era script

--- src/main/sql/vocabulary_loading/update_stcm_source_concept_id.sql
+++ src/main/sql/vocabulary_loading/update_stcm_source_concept_id.sql
@@ -1,14 +1,14 @@
 /**
 Add source_concept_id to the source_to_concept_map mappings
 */
-UPDATE @vocab_schema.source_to_concept_map
+UPDATE @vocabulary_schema.source_to_concept_map
 SET source_concept_id = concept_id
 FROM (
        SELECT
          concept_id,
          concept_code,
          vocabulary_id
-       FROM @vocab_schema.concept
+       FROM @vocabulary_schema.concept
        WHERE concept_id >= 2000000000
      ) concept
 WHERE source_to_concept_map.source_code = concept.concept_code AND

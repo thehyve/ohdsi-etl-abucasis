63896019c3f4c87b19b0ee2ddb6679b3f7cdf151
egarcialara <e.oohla@hotmail.com>
2021-08-23 18:23:18 +0200

    datetime to date in v5 condition_era test

--- src/test/end_to_end/test_cases/condition_era.R
+++ src/test/end_to_end/test_cases/condition_era.R
@@ -8,5 +8,5 @@ add_tb_ante_cmbd(id=800,numsipcod='A800', fecha_ingreso='2013-01-01', fecha_alta
 add_tb_diag_juntos(id=800,numsipcod='A800', fecha_inicio='2013-01-01', fecha_fin='2013-02-01',
                    origen = 'C',peso_grd=NULL, orden=NULL)
 expect_condition_era(person_id = lookup_person('person_id', person_source_value = 'A800'),
-                            condition_era_start_datetime = '2013-01-01 00:00:00.000000',
-                            condition_era_end_datetime = '2013-02-01 00:00:00.000000')
+                            condition_era_start_date = '2013-01-01 00:00:00.000000',
+                            condition_era_end_date = '2013-02-01 00:00:00.000000')
